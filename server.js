f397b1fc344e0efaf6caf18bf1582b31a9fe1932
Dan <daniel.murphy.1@gmail.com>
2020-10-10 15:42:59 -0400

    added concurrently and build watch

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+app.use("/", express.static(path.join(__dirname, client/build)))
 
 app.get("/", (req, res) => {
     res.send("Working")
