219803caf9b8137a8bd0ab60d3152c79340509c4
Dan <daniel.murphy.1@gmail.com>
2020-10-11 10:54:47 -0400

    pushed changes that didn't push last commit

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
 
-app.use("/", express.static(path.join(__dirname, client/build)))
+app.use("/", express.static(path.join(__dirname, "client/build")));
+app.use("/search", express.static(path.join(__dirname, "client/build")));
+app.use("/random", express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
     res.send("Working")
