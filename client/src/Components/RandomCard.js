8213504ebbd586bba41ff77d0c1c11c76cf107d8
Dan <daniel.murphy.1@gmail.com>
2020-10-17 11:22:09 -0400

    added dynamic random cards

--- client/src/Components/RandomCard.js
+++ client/src/Components/RandomCard.js
@@ -1,17 +1,17 @@
 
 import React from 'react';
 
-function RandomCard() {
+function RandomCard(props) {
     return(
         <div className="card w-25 btn m-2 d">   
             <img 
-                src={"https://pbs.twimg.com/profile_images/1278183948279922690/ybnDHXn7_normal.jpg"} 
+                src={props.url} 
                 alt="profile-image" 
                 className="img-fluid rounded-circle mx-auto" 
                 style={{height: 60, width: 60}}>
             </img>
-            <h4>Playstation</h4>
-            <h4>@Playstation</h4>
+            <h4>{props.name}</h4>
+            <h4>{props.userName}</h4>
 
         </div>
     )
--- client/src/Pages/Random.js
+++ client/src/Pages/Random.js
@@ -31,6 +31,10 @@ class Random extends React.Component{
         //create array of objects for 5 favorites
         //in the container div, map over the array to generate 5 cards dynamically
         console.log(randomData[0].userName)
+        // const data =[];
+        // for(let i =1; i <=5; i++){
+        //     data.push(i);
+        // }
         return(
             
             <div className="container">
@@ -38,11 +42,14 @@ class Random extends React.Component{
                     <h4>Choose One of My Favorite Users to Get One of Their Random Tweets</h4>
                 </div>
                 <div className="container d-flex justify-content-between flex-wrap">
-                    <RandomCard />
-                    <RandomCard />
-                    <RandomCard />
-                    <RandomCard />
-                    <RandomCard />
+                    {randomData.map(random =>{
+                        return <RandomCard
+                            key={random.id}
+                            name={random.name}
+                            userName={random.userName}
+                            url={random.url}
+                            />
+                    })}
                 </div>
                 <Modal isModal={this.state.isModal} closeModal={this.closeModal} />
                 <button onClick={this.handleClick} className="btn btn-danger">Test</button>
