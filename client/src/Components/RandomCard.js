ffdb417889b1e36a7b08975b373c77400b0e9e0d
Dan <daniel.murphy.1@gmail.com>
2020-10-19 21:44:37 -0400

    clean up whitespace and indents on files for random functionality before merging to main

--- client/src/Components/RandomCard.js
+++ client/src/Components/RandomCard.js
@@ -4,11 +4,6 @@ import React from 'react';
 function RandomCard(props) {
     return(
         <div className="card w-25 btn m-2 d" onClick={()=>{props.handleClick(props.userName, props.name)}} >
-            {/* onClick = {(e) => {props.handleClick(props.username)}} 
-                //this needs to go to the div for clicking need to change and pass the handleCLick method on Random page
-                @ symbol in the variable will be a problem...need to remove it from the variable and hardcode it into the card
-                iterate over the returned tweets to generate random with random number generator 
-                use backend from the /api/user/search likely*/}
             <img 
                 src={props.url} 
                 alt="profile-image" 
