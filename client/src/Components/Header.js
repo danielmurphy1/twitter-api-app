3f4d73321c921eff2ef6ba4b05be4c24e838eda8
Dan <daniel.murphy.1@gmail.com>
2020-10-27 21:19:53 -0400

    added images of pages, added sticky top to Navbar

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -3,7 +3,7 @@ import { Navbar, Nav } from "react-bootstrap";
 
 function Header(){
     return(
-        <Navbar className="App-header" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar className="App-header" collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
             <Navbar.Brand href="#home">{<img src={require("../images/bird.jpg")} style={{height: 60, width: 60}}></img>}</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
